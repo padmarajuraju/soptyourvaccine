62666cdf5b030127c2dd7943af50542efc851666
padmarajuraju <111412403+padmarajuraju@users.noreply.github.com>
2022-09-19 12:26:25 +0530

    Update Editprofile.js

--- product-webapp/web-app/src/components/vaccination-center/Editprofile.js
+++ product-webapp/web-app/src/components/vaccination-center/Editprofile.js
@@ -20,7 +20,7 @@ function EditProfile() {
 
     const getUserDetailsById = (id) => {
         // var url = "http://localhost:8080/user-service/api/v1/vaccination-center/" + id;
-        var url = "https://spotyourvaccine.stackroute.io/user-service/api/v1/vaccination-center/" + id;
+        var url = "http://3.142.208.221:8080/user-service/api/v1/vaccination-center/" + id;
         return fetch(url,
             {
                 headers: {
