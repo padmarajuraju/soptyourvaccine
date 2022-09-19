ff475b3c782f3441cb11c8434566dbb36cd319b0
padmarajuraju <111412403+padmarajuraju@users.noreply.github.com>
2022-09-19 18:42:16 +0530

    Update vc-registration.js

--- product-webapp/web-app/src/components/vaccination-center/vc-registration.js
+++ product-webapp/web-app/src/components/vaccination-center/vc-registration.js
@@ -37,7 +37,7 @@ function VCRegistration() {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({...addCenter})
         };
-        fetch('http://3.142.208.221:4200/user-service/api/v1/vaccination-center/addVaccineCenter', requestOptions)
+        fetch('http://3.142.208.221:8080/user-service/api/v1/vaccination-center/addVaccineCenter', requestOptions)
             .then((result) => {
                 if (result.status === 201) {
                     setOpen(true);
